c01710f21324dbbc47d1574929410d5af156a03f
Isa Giongo <isagiongo@gmail.com>
2020-01-25 18:20:45 -0300

    Cria service para calculo

--- pricing-service/src/main/java/com/isagiongo/cloud/pricingservice/v1/controllers/PriceController.java
+++ pricing-service/src/main/java/com/isagiongo/cloud/pricingservice/v1/controllers/PriceController.java
@@ -1,13 +1,11 @@
 package com.isagiongo.cloud.pricingservice.v1.controllers;
 
+import com.isagiongo.cloud.pricingservice.services.PriceCalculator;
 import com.isagiongo.cloud.pricingservice.v1.controllers.dtos.PriceRequest;
 import com.isagiongo.cloud.pricingservice.v1.controllers.dtos.PriceResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -15,8 +13,16 @@ import java.math.BigDecimal;
 @RequestMapping("/v1")
 public class PriceController {
 
-    @GetMapping("/prices")
-    public ResponseEntity<PriceResponse> getPriceAmount(@RequestBody PriceRequest priceRequest) {
-        return new ResponseEntity<PriceResponse>(new PriceResponse(BigDecimal.ZERO), HttpStatus.OK);
+    private final PriceCalculator priceCalculator;
+
+    public PriceController(PriceCalculator priceCalculator) {
+        this.priceCalculator = priceCalculator;
     }
+
+    @PostMapping("/prices")
+    public ResponseEntity<PriceResponse> getPriceAmount(@RequestBody PriceRequest priceRequest) {
+        return new ResponseEntity<>(new PriceResponse(priceCalculator.calculatePrice(priceRequest.getStart(), priceRequest.getEnd(), priceRequest.getPlan())),
+                HttpStatus.OK);
+    }
+
 }
